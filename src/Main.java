ace427443b134c183415fc6203a1dc13b09dea9d
Mason Beale <mbeale@cedarville.edu>
2022-12-31 18:04:11 -0500

    Added test and code for appending text

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
-    public static String journal = "";
+    public String journal = "";
 
     public static void main(String[] args) {
 
     }
-    public static void write(String input){
-        journal = input;
+    public void write(String input){
+        journal += input;
     }
+
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,12 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
+    private Main mainClass;
+    @Before
+    public void setUp() throws Exception {
+        mainClass = new Main();
+    }
+
     @Test
     public void writingTest() throws IOException {
-        Main.write("Test Statement");
-        assertEquals("Test Statement", Main.journal);
+        mainClass.write("Test Statement");
+        assertEquals("Test Statement", mainClass.journal);
+    }
+
+    @Test
+    public void appendingTest() throws IOException {
+        mainClass.write("Test Statement");
+        mainClass.write(" And the appended statement");
+        assertEquals("Test Statement And the appended statement", mainClass.journal);
     }
 }
