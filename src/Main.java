b7fe1d2d02a0dde24256e2f14b16182be4024e83
Mason Beale <mbeale@cedarville.edu>
2023-01-19 18:56:47 -0500

    Refactored

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,31 @@ public class Main {
     public void write(String input){
         char[] actual_line = new char[input.length()];
         for(int i = 0; i < input.length(); i++){
-            if(ptDurability > 0){
-                actual_line[i] = input.charAt(i);
-            }
-            else{
-                actual_line[i] = ' ';
-            }
-            if(input.charAt(i) != '\n' && input.charAt(i) != ' ') {
-                if (Character.isUpperCase(input.charAt(i))) {
-                    ptDurability -= 2;
-
-                }
-                else {
-                    // Applies to non-letters, which can't be upper/lower
-                    ptDurability--;
-                }
-            }
+            insertInArray(input, actual_line, i);
+            degradePencil(input, i);
         }
         String result = new String(actual_line);
-
         journal += result;
+    }
+
+    private void insertInArray(String input, char[] actual_line, int i) {
+        if(ptDurability > 0){
+            actual_line[i] = input.charAt(i);
+        }
+        else {
+            actual_line[i] = ' ';
+        }
+    }
+
+    private void degradePencil(String input, int i) {
+        if(input.charAt(i) != '\n' && input.charAt(i) != ' ') {
+            if (Character.isUpperCase(input.charAt(i))) {
+                ptDurability -= 2;
+            }
+            else {
+                // Applies to non-letters, which can't be upper/lower
+                ptDurability--;
+            }
+        }
     }
 }
