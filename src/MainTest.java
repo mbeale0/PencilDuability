130f272d411be9b24ae6cca072702a5df5dc686c
Mason Beale <mbeale@cedarville.edu>
2023-01-24 18:54:35 -0500

    refactored pencil into its own class

--- src/MainTest.java
+++ src/MainTest.java
@@ -5,48 +5,47 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
-    private Main mainClass;
+    private Pencil pencil;
     @Before
     public void setUp() throws Exception {
-        mainClass = new Main();
-        mainClass.ptDurability = 50;
+        pencil = new Pencil(50);
     }
 
     @Test
     public void writingTest() throws IOException {
-        mainClass.write("Test Statement");
-        assertEquals("Test Statement", mainClass.journal);
+        pencil.write("Test Statement");
+        assertEquals("Test Statement", pencil.getJournal());
     }
 
     @Test
     public void appendingTest() throws IOException {
-        mainClass.write("Test Statement");
-        mainClass.write(" And the appended statement");
-        assertEquals("Test Statement And the appended statement", mainClass.journal);
+        pencil.write("Test Statement");
+        pencil.write(" And the appended statement");
+        assertEquals("Test Statement And the appended statement", pencil.getJournal());
     }
 
     @Test
     public void degradationDuringWritingTest() throws IOException {
-        mainClass.write("15 non spaces long");
-        assertEquals(35, mainClass.ptDurability);
+        pencil.write("15 non spaces long");
+        assertEquals(35, pencil.getPtDurability());
     }
 
     @Test
     public void uppercaseLettersDegradesProperlyTest() throws IOException {
-        mainClass.write("CAPITAL");
-        assertEquals(36, mainClass.ptDurability);
+        pencil.write("CAPITAL");
+        assertEquals(36, pencil.getPtDurability());
     }
 
     @Test
     public void lowercaseLettersDegradesProperlyTest() throws IOException {
-        mainClass.write("lower!?");
-        assertEquals(43, mainClass.ptDurability);
+        pencil.write("lower!?");
+        assertEquals(43, pencil.getPtDurability());
     }
 
     @Test
     public void stopWritingAfterDegradationTest() throws IOException {
-        mainClass.write("Boy I sure do love my new pencil, I really hope it lasts forever");
+        pencil.write("Boy I sure do love my new pencil, I really hope it lasts forever");
 
-        assertEquals("Boy I sure do love my new pencil, I really hope it lasts for    ", mainClass.journal);
+        assertEquals("Boy I sure do love my new pencil, I really hope it lasts for    ", pencil.getJournal());
     }
 }
---
+++ src/Pencil.java
@@ -0,0 +1,47 @@
+public class Pencil {
+    private String journal = "";
+    private int ptDurability = 0;
+
+    public Pencil(int ptDurability) {
+        this.ptDurability = ptDurability;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public int getPtDurability() {
+        return ptDurability;
+    }
+
+    public void write(String input){
+        char[] actual_line = new char[input.length()];
+        for(int i = 0; i < input.length(); i++){
+            insertInArray(input, actual_line, i);
+            degradePencil(input, i);
+        }
+        String result = new String(actual_line);
+        journal += result;
+    }
+
+    private void insertInArray(String input, char[] actual_line, int i) {
+        if(ptDurability > 0){
+            actual_line[i] = input.charAt(i);
+        }
+        else {
+            actual_line[i] = ' ';
+        }
+    }
+
+    private void degradePencil(String input, int i) {
+        if(input.charAt(i) != '\n' && input.charAt(i) != ' ') {
+            if (Character.isUpperCase(input.charAt(i))) {
+                ptDurability -= 2;
+            }
+            else {
+                // Applies to non-letters, which can't be upper/lower
+                ptDurability--;
+            }
+        }
+    }
+}
